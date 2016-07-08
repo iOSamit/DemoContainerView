4c3de40a7503f9179fa5fcf5b21b38b99106ae50
amit <amit400gupta@gmail.com>
2016-07-08 13:18:58 +0530

    Inital Setup for container view demo in swift

---
+++ DemoContainerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoContainerView
+//
+//  Created by Amit on 08/07/2015 SAKA.
+//  Copyright © 2016 Dream World. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
